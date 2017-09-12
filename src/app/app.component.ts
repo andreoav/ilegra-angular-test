0214e491339488c62324e22b857171b71c7dff3c
Andreo Vieira <andreo.vieira@meta.com.br>
2017-09-12 17:31:00 -0300

    Listagem estática de filmes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Movie } from './movies/movie.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app';
+
+  movies: Movie[] = [
+    new Movie('Interestelar', 'Good Movie'),
+    new Movie('IT (2017)', 'Awesome Movie'),
+    new Movie('Central do Brazil', 'Awesome Movie'),
+    new Movie('Central do Brazil', 'Awesome Movie'),
+    new Movie('Central do Brazil', 'Awesome Movie'),
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MoviesListComponent } from './movies/movies-list/movies-list.component';
+import { MovieItemComponent } from './movies/movie-item/movie-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesListComponent,
+    MovieItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
